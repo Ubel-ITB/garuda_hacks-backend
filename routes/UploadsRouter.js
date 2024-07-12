471333646087f40fbccb3d1618abfd860a7cf86b
Dave <d06163606@gmail.com>
2024-07-12 10:45:42 +0700

    fix merge conflict

--- routes/UploadsRouter.js
+++ routes/UploadsRouter.js
@@ -10,11 +10,7 @@ UploadsRouter.get("/:folderName/:fileName", (req, res) => {
 
 UploadsRouter.post("/:folderName/:fileName", MulterUpload.single("file"), (req, res) => {
   const { folderName } = req.params;
-<<<<<<< HEAD
   const uploadedFilePath = `/uploads/${folderName}/${req?.file?.filename}`;
-=======
-  const uploadedFilePath = `/uploads/${folderName}/${req.file.filename}`;
->>>>>>> 1d86e710fabe8563ec1f30b27dd946eb2c8583b3
   res.status(201).json({ url: process.env.BASE_URL + uploadedFilePath });
 });
 
